0b0c074b576c327a14dcedf9cd7e4e1c12dc9cda
Hân <tranhantgdd2020@gmail.com>
2025-06-29 19:49:47 +0700

    update

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -3,8 +3,17 @@ const { MongoClient } = require("mongodb");
 class MongoDB {
     static connect = async (uri) => {
         if (this.client) return this.client;
-        this.client = await MongoClient.connect(uri);
-        return this.client;
+        try {
+            this.client = await MongoClient.connect(uri, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            console.log("✅ Connected to MongoDB");
+            return this.client;
+        } catch (error) {
+            console.error("❌ MongoDB connection failed:", error.message);
+            return null; // hoặc throw error nếu bạn muốn dừng hẳn
+        }
     }
 }
 
